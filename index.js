8237d7a5e40b35ce15cae7c263005818dca49e02
Volodymyr Kulyk <volodymyr.kulyk@devico.io>
2020-07-30 19:26:03 +0300

    move the code into separate functions. fix the button for completing all todos

--- index.js
+++ index.js
@@ -1,57 +1,50 @@
-const app = document.querySelector('.app');
+const getToDo = (todo) => {
+  const li = document.createElement('li');
 
-const addToDo = (todo) => {
-  if (todo.title) {
-    const li = document.createElement('li');
+  li.classList.add('app-todo');
+  li.textContent = todo.title;
+  li.dataset.id = todo.id;
 
-    li.classList.add('app-item');
-    li.textContent = todo.title;
+  todo.completed ? li.classList.add('completed') : li.classList.remove('completed');
 
-    todo.completed ? li.classList.add('completed') : li.classList.remove('completed');
-
-    li.addEventListener('click', (e) => {
-      todo.completed = !todo.completed;
-      e.target.classList.toggle('completed');
-    })
-
-    const remover = document.createElement('button');
-
-    remover.textContent = 'x';
-
-    remover.classList.add('app-item-remove', 'button');
-
-    remover.addEventListener('click', (e) => {
-      e.target.parentNode.remove();
-    });
-
-    li.append(remover);
-
-    return li;
-  }
-}
-
-const render = () => {
-  while (app.firstChild) {
-    app.firstChild.remove();
-  }
-
-  // Add checkall button
-  const checkall = document.createElement('span');
-
-  checkall.classList.add('material-icons', 'app-checkall');
-  checkall.textContent = 'check_box';
-
-  checkall.addEventListener('click', (e) => {
-    todos.forEach((todo) => {
-      todo.completed = true;
-    });
-
-    render();
+  li.addEventListener('click', (e) => {
+    todo.completed = !todo.completed;
+    e.target.classList.toggle('completed');
   })
 
-  app.append(checkall);
+  const remover = document.createElement('button');
 
-  // Add main input
+  remover.textContent = 'x';
+
+  remover.classList.add('app-todo-remove', 'button');
+
+  // change parentNode
+  remover.addEventListener('click', (e) => {
+    const id = +li.dataset.id;
+
+    todos = todos.filter((todo) => todo.id !== id);
+
+    li.remove();
+  });
+
+  li.append(remover);
+
+  return li;
+}
+
+const renderToDos = (ul) => {
+  while (ul.firstChild) {
+    ul.firstChild.remove();
+  }
+
+  todos.forEach((todo) => {
+    const li = getToDo(todo);
+
+    ul.append(li);
+  });
+}
+
+const getNewToDoInput = (ul) => {
   const input = document.createElement('input');
 
   input.type = 'text';
@@ -61,39 +54,70 @@ const render = () => {
   input.classList.add('app-new-todo', 'input');
 
   input.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-      const todo = {
-        title: e.target.value,
-        completed: false,
-      };
+    if (e.keyCode === 13 && e.target.value) {
+      const id = todos[todos.length - 1].id + 1;
+      const title = e.target.value;
+      const completed = false;
+
+      const todo = { id, title, completed };
 
       todos.push(todo);
 
-      const li = addToDo(todo);
+      const li = getToDo(todo);
 
-      if (li) {
-        ul.append(li);
-      }
+      e.target.value = '';
+
+      ul.append(li);
     }
   })
 
-  app.append(input);
+  return input;
+}
 
-  // Add list
+const getCheckAllBtn = (ul) => {
+  const checkAll = document.createElement('span');
+
+  checkAll.classList.add('material-icons', 'app-checkall');
+  checkAll.textContent = 'check_box';
+
+  let checked = false;
+
+  checkAll.addEventListener('click', (e) => {
+    checked = !checked;
+
+    todos.forEach((todo) => {
+      todo.completed = checked;
+    });
+
+    renderToDos(ul);
+  })
+
+  return checkAll;
+}
+
+const render = () => {
+  const root = document.querySelector('#root');
+
+  root.innerHTML = rootTemplate;
+
+  const app = document.querySelector('.app');
+
   const ul = document.createElement('ul');
 
-  ul.classList.add('app-list');
+  ul.classList.add('app-todos');
 
-  todos.forEach((todo) => {
-    const li = addToDo(todo);
+  renderToDos(ul);
 
-    ul.append(li);
-  });
+  const input = getNewToDoInput(ul);
 
+  const checkAllBtn = getCheckAllBtn(ul);
+
+  app.append(checkAllBtn);
+  app.append(input);
   app.append(ul);
 
   // Add footer
-  const footer = document.createElement('div');
+  // const footer = document.createElement('div');
 }
 
 render();
---
+++ templates.js
@@ -0,0 +1,15 @@
+const rootTemplate = `<div class="wrapper">
+  <header class="header">
+    <div class="container">
+      <h1 class="app-title title">todos</h1>
+    </div>
+  </header>
+  <main class="main">
+    <section class="section todoapp">
+      <div class="container">
+        <div class="app">
+        </div>
+      </div>
+    </section>
+  </main>
+  </div>`;
