79d8bf420c9d7e05ceddda23135235e066636695
Volodymyr Kulyk <volodymyr.kulyk@devico.io>
2020-08-18 17:23:54 +0300

    formatted config

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,71 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const HtmlWebpackInjector = require('html-webpack-injector');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackInjector = require("html-webpack-injector");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const path = require('path');
+const path = require("path");
 
 module.exports = () => ({
-  context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, "src"),
   entry: {
-    index: './index.js'
+    index: "./index.js",
   },
   output: {
-    filename: '[name].[contenthash].bundle.js',
-    path: path.resolve('build'),
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve("build"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
-            loader: 'file-loader',
-            options: { outputPath: 'fonts', },
-          }
-        ]
+            loader: "file-loader",
+            options: { outputPath: "fonts" },
+          },
+        ],
       },
       {
         test: /\.(jpg|png|svg|gif)$/,
-        use: [{
-          loader: 'file-loader',
-          options: { outputPath: 'images', },
-        }],
+        use: [
+          {
+            loader: "file-loader",
+            options: { outputPath: "images" },
+          },
+        ],
       },
       {
-        test: /\.js$/, exclude: /node_modules/, loader: {
-          loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', {
-              'plugins': ['@babel/plugin-proposal-class-properties']
-            }]
-          }
-        }
-      }
-    ]
+            presets: [
+              "@babel/preset-env",
+              {
+                plugins: ["@babel/plugin-proposal-class-properties"],
+              },
+            ],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
-      chunks: ["index"]
+      template: "./index.html",
+      chunks: ["index"],
     }),
     new HtmlWebpackInjector(),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
   ],
   devServer: {
